9529693b33f0ce2ab9bb59a7aeb5df3b44f3325d
meh <meh@schizofreni.co>
2013-04-23 22:36:09 +0200

    Add package file

---
+++ package.exs
@@ -0,0 +1,9 @@
+Expm.Package.new(
+  name: "finalizer",
+  description: "Define a GC finalizer",
+  version: "0.0.1",
+  licenses: [[name: "WTFPL"]],
+  keywords: ["gc"],
+  maintainers: [[name: "meh.", email: "meh@schizofreni.co"]],
+  repositories: [[github: "meh/elixir-finalizer", tag: "v0.0.1"]]
+)
