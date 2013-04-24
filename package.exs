9734c00cec874c22349a00f97e71b2efb26b3713
meh <meh@schizofreni.co>
2013-04-24 21:18:18 +0200

    Bump version

--- package.exs
+++ package.exs
@@ -1,9 +1,9 @@
 Expm.Package.new(
   name: "finalizer",
   description: "Define a GC finalizer",
-  version: "0.0.2",
+  version: "0.0.3",
   licenses: [[name: "WTFPL"]],
   keywords: ["gc"],
   maintainers: [[name: "meh.", email: "meh@schizofreni.co"]],
-  repositories: [[github: "meh/elixir-finalizer", tag: "v0.0.2"]]
+  repositories: [[github: "meh/elixir-finalizer", tag: "v0.0.3"]]
 )
