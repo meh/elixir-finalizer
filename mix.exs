1977ea3e8b9c89b3889ff640a35ce0c2d5777f55
Christoph Grabo <chris@dinarrr.com>
2014-03-23 15:07:29 +0100

    Remove comments

--- mix.exs
+++ mix.exs
@@ -7,13 +7,10 @@ defmodule Finalizer.Mixfile do
       deps: deps ]
   end
 
-  # Configuration for the OTP application
   def application do
     [ mod: { Finalizer.Manager, [] } ]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     [ { :resource, github: "tonyrog/resource" } ]
   end
